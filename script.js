651ecb252c7d3f9d766c73202157fc6274804932
kendy-karakawa <kendy.karakawa@gmail.com>
2022-10-26 16:47:41 -0300

    inserir mensagem do servidor nao esta funcionando

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 let mensagens = [
 
-    { from : "chimbinha" , to : "dsds" , text :" mensagem digitada ", type : "message" , time : "05:47:26" },
-    { from : "chimbinha" , to : "Todos" , text :" sai da sala... ", type : "status" , time : "05:47:45" },
-    { from : "chimbinha" , to : "Todos" , text :" entra na sala... ", type : "status" , time : "05:47:58" },
-    { from : "chimbinha" , to : "xyz" , text :" mensagem digitada" , type : "private_message" , time : "05:48:04" },
-    { from : "chimbinha" , to : "Todos" , text : "sai da sala..." , type : "status" , time : "05:48:15" },
-    { from : "chimbinha" , to : "Todos" , text : "entra na sala..." , type : "status" , time : "05:48:18" },
-    { from : "chimbinha" , to : "xyz" , text : "mensagem digitada" , type : "message" , time : "05:48:23" },
-    { from : "chimbinha" , to : "Todos" , text : "sai da sala..." , type : "status" , time : "05:48:30" }
+    
 ];
 
-/*const pegarNoServidor = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+const pegarNoServidor = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
 pegarNoServidor.then(chegou)
 
 function chegou(resposta){
     console.log(resposta.data)
     mensagens = resposta.data
-}*/
+}
 
 addMensagem()
 
